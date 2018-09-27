4bcf72f866472310c46b8da7bf84082d977f0022
Eugene Tyurin <eugene@tyurin.com>
2018-09-26 20:55:04 -0500

    updated project.clj, fixed basic-test matrix errors

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.mikera/core.matrix "0.32.1"]
-                 [net.mikera/vectorz-clj "0.28.0"]
-                 [incanter "1.5.6"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [net.mikera/core.matrix "0.62.0"]
+                 [net.mikera/vectorz-clj "0.48.0"]
+                 [incanter "1.9.3"]])
